24002f30cab2f71eac33aafa87c6bb18e12cd904
Salihapeker <salihapeker3@gmail.com>
2024-05-15 18:24:54 +0300

    Paint WFA
    Temel C# Windows Form Application projesi

---
+++ UygPaint/UygPaint/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace UygPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Daire Çiz",
+            "Üçgen Çiz",
+            "Dikdörtgen Çiz",
+            "Serbest Çizim"});
+            this.comboBox1.Location = new System.Drawing.Point(89, 76);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(281, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(508, 404);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(99, 154);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(143, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(-1, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kalem Kalınlığı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Çizim Seç";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Renk Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ UygPaint/UygPaint/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UygPaint
+{
+    public partial class Form1 : Form
+    {
+        int ts = 0;
+        int[] xler = new int[4];
+        int[] yler = new int[4];
+        Pen varsayılanKalem = new Pen(Color.Black, 4);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+
+
+            Graphics cizimalanı = panel1.CreateGraphics();
+            varsayılanKalem = new Pen(colorDialog1.Color, (float)numericUpDown1.Value);
+            
+            if (comboBox1.SelectedIndex == 0)
+            {
+                cizimalanı.DrawEllipse(varsayılanKalem, e.X, e.Y, 20, 20);
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                xler[ts] = e.X;
+                yler[ts] = e.Y;
+                ts++;
+
+                if (ts == 3)
+                {
+
+                    Graphics cizimalan = panel1.CreateGraphics();
+                    cizimalan.DrawLine(varsayılanKalem, xler[0], yler[0], xler[1], yler[1]);
+                    cizimalan.DrawLine(varsayılanKalem, xler[1], yler[1], xler[2], yler[2]);
+                    cizimalan.DrawLine(varsayılanKalem, xler[2], yler[2], xler[0], yler[0]);
+                    ts = 0;
+                }
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                cizimalanı.DrawRectangle(varsayılanKalem, e.X, e.Y, 10, 30);
+            }
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (comboBox1.SelectedIndex == 3 && e.Button == MouseButtons.Left)
+            {
+                Graphics cizimalanı = panel1.CreateGraphics();
+                varsayılanKalem = new Pen(colorDialog1.Color, (float)numericUpDown1.Value);
+                cizimalanı.DrawEllipse(varsayılanKalem, e.X, e.Y, 5, 5);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                varsayılanKalem.Color = colorDialog1.Color;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            varsayılanKalem.Width = (float)numericUpDown1.Value;
+        }
+
+     
+    }
+}
